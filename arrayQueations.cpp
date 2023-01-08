af0f96ce9a95de3f6bbbeb4fa2b240945d424e51
Rohan Rathi <103847468+rohanRathi999@users.noreply.github.com>
2023-01-08 23:15:35 +0530

    commit

---
+++ arrayQueations.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+//swapping alternate values of an array
+// void altSwap(int arr[], int size){
+//     for(int i=0;i<size;i+=2){
+//         if(i+1<size){
+//             swap(arr[i],arr[i+1]);
+//         }
+//     }
+// }
+// void printArray(int arr[],int size){
+//     for(int i=0;i<size;i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+// int main(){
+//     int evenArr[6]={1,2,3,4,5,6};
+//     int oddArr[5]={2,4,6,8,0};
+
+//     altSwap(evenArr,6);
+//     altSwap(oddArr,5);
+
+//     printArray(evenArr,6);
+//     cout<<endl;
+//     printArray(oddArr,5);
+// }
+
+
+
+// finding the unique element in array 
+// int unique(int arr[], int size){
+//     int ans=0;
+//     for(int i=0;i<size;i++){
+//         ans = ans^arr[i];
+//     }
+//     return ans;
+// }
+// int main(){
+//     int arr[]={2,4,2,4,5};
+
+//     unique(arr,5);
+
+//     cout<<unique;
+// }
+
+
+// finding duplicate number 
+
+
