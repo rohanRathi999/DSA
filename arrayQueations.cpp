82496324739e9c0f33061df3f843d707377d2fc6
Rohan Rathi <103847468+rohanRathi999@users.noreply.github.com>
2023-01-11 23:36:10 +0530

    commit

--- arrayQueations.cpp
+++ arrayQueations.cpp
@@ -29,19 +29,18 @@ using namespace std;
 
 
 // finding the unique element in array 
-int unique(int arr[], int size){
-    int ans=0;
-    for(int i=0;i<size;i++){
-        ans = ans^arr[i];
-    }
-    return ans;
-}
-int main(){
-    int arr[]={2,4,2,4,5};
-    cout << unique(arr,5);
-}
+// int unique(int arr[], int size){
+//     int ans=0;
+//     for(int i=0;i<size;i++){
+//         ans = ans^arr[i];
+//     }
+//     return ans;
+// }
+// int main(){
+//     int arr[]={2,4,2,4,5};
+//     cout << unique(arr,5);
+// }
 
 
-// finding duplicate number 
 
 
