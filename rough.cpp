0d9b56df97dea69036dcb0dc7e74b34286b20549
Rohan Rathi <103847468+rohanRathi999@users.noreply.github.com>
2023-01-23 23:26:50 +0530

    Update rough.cpp

--- rough.cpp
+++ rough.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main() {
 	// your code goes here
-	int n;
-    cin>>n;
-    int A[n],B[n];
-    for (int i=0;i<n;i++){
+	int N;
+    cin>>N;
+    int A[N],B[N];
+    for (int i=0;i<N;i++){
         cin>>A[i];
     }
-    for (int i=0;i<n;i++){
+    for (int i=0;i<N;i++){
         cin>>B[i];
     }
     
     int max=0;
-    for(int i=1;i<n;i++){
+    for(int i=1;i<N;i++){
         if(A[max]<A[i]){
             max=i;
         }
