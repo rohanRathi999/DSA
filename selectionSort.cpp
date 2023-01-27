53c2ca128af372d2d3fe7b0f06fa9d175bdba7df
Rohan Rathi <103847468+rohanRathi999@users.noreply.github.com>
2023-01-27 16:23:39 +0530

    commit

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -9,7 +9,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    for(int i=0;i<n-1;i++){
+    for(int i=0;i<n-1;i++){ // assume that first index have min element, now traverse the complete array with another loop starting from the next index of the minIndex to check whether there is any index having number lesser than minindex value, now least value element will be at first index now for next index, proceed the same method till n-1 because last element will be the greatest no need to check that.
         int minIndex=i;
 
         for(int j=i+1;j<n;j++){
