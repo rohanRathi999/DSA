8a2341fb378812f7870722510553c63dd84e0985
Rohan Rathi <103847468+rohanRathi999@users.noreply.github.com>
2023-01-28 22:25:20 +0530

    commit

---
+++ bubbleSort.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=1;i<n;i++){
+        //this i wala loop is for rounds and rounds are from 1 to n-1
+        for(int j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
